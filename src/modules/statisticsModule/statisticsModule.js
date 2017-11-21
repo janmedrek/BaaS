a9dc8b91788e4eef6d889920fc2a0626d3aa0950
Jan Medrek <jan.medrek@sap.com>
2017-11-21 11:06:24 +0100

    gitignore update and statistics module rework in progress

--- src/modules/statisticsModule/statisticsModule.js
+++ src/modules/statisticsModule/statisticsModule.js
@@ -2,16 +2,46 @@ const datastoreFacade = require('../datastoreFacade/datastoreFacade');
 
 const statisticsModule = {};
 
+const mergeStatistics = async (obj1, obj2) => {
+    let keys = Object.keys(obj1);
+
+    const result = {};
+
+    keys.map((currentKey) => {
+        if (obj2[`${currentKey}`]) {
+            result[`${currentKey}`] = obj1[`${currentKey}`] + obj2[`${currentKey}`];
+        } else {
+            result[`${currentKey}`] = obj1[`${currentKey}`];
+        }
+
+        return true;
+    });
+
+    keys = Object.keys(obj2);
+
+    keys.map((currentKey) => {
+        if (!result[`${currentKey}`]) {
+            result[`${currentKey}`] = obj2[`${currentKey}`];
+        }
+
+        return true;
+    });
+
+    return result;
+};
+
 statisticsModule.getStatistics = async username => datastoreFacade.getStatistics(username);
 
 statisticsModule.saveStatistics = async (username, statistics) => {
-    try {
-        const result = await datastoreFacade.saveStatistics(username, statistics);
-        return result;
-    } catch (err) {
-        throw err;
-    }
+    // TODO:
+    const result = await mergeStatistics({ lol: 1, lel: 2, lul: 3 }, { lol: 2, lil: 4, lulu: 3 });
+    console.log(result);
+    // try {
+    //     const result = await datastoreFacade.saveStatistics(username, statistics);
+    //     return result;
+    // } catch (err) {
+    //     throw err;
+    // }
 };
-
 
 module.exports = statisticsModule;
